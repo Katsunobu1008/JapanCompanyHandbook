a1565bd6e3f786bddeff853b6e58c960749b665b
katsunobu1008 <katsunobu1008@gmail.com>
2025-06-10 16:05:09 +0900

    最初からやり直す

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import CompanyListPage from './pages/CompanyListPage';
-import CompanyDetailPage from './pages/CompanyDetailPage';
-import Header from './components/Header';
-import './index.css';
+import logo from './logo.svg';
+import './App.css';
+
 function App() {
   return (
-    <Router>
-      <Header />
-      <div className="container">
-        <Routes>
-          <Route path="/" element={<CompanyListPage />} />
-          <Route path="/company/:id" element={<CompanyDetailPage />} />
-        </Routes>
-      </div>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
+
 export default App;
